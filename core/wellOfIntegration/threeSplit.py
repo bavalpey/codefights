b04c8fc2ebc98b757980d5d7741fa763d4b53679
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 15:13:29 -0500

    Update threeSplit.py

--- core/wellOfIntegration/threeSplit.py
+++ core/wellOfIntegration/threeSplit.py
@@ -1,5 +1,9 @@
 """
-You have a long strip of paper with integers written on it in a single line from left to right. You wish to cut the paper into exactly three pieces such that each piece contains at least one integer and the sum of the integers in each piece is the same. You cannot cut through a number, i.e. each initial number will unambiguously belong to one of the pieces after cutting. How many ways can you do it?
+You have a long strip of paper with integers written on it in a single line from left to right.
+You wish to cut the paper into exactly three pieces such that each piece contains at least one
+integer and the sum of the integers in each piece is the same. You cannot cut through a number,
+i.e. each initial number will unambiguously belong to one of the pieces after cutting. How many
+ways can you do it?
 It is guaranteed that the sum of all elements in the array is divisible by 3.
 Example
 For a = [0, -1, 0, -1, 0, -1], the output should be
