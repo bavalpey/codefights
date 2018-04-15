9adb15a0fe80d8309a21b6b58892688e13012367
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 17:22:01 -0500

    Create createArray.py

---
+++ core/listForestEdge/createArray.py
@@ -0,0 +1,10 @@
+"""
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+For size = 4, the output should be
+  createArray(size) = [1, 1, 1, 1].
+"""
+def createArray(size):
+    return [1]*size
