33953aea81ad7c078ef4765699682f8a54363110
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 15:24:50 -0500

    Update swapAdjacentBits.py

--- core/cornerOf0sAnd1s/swapAdjacentBits.py
+++ core/cornerOf0sAnd1s/swapAdjacentBits.py
@@ -7,12 +7,12 @@ Example
 For n = 13, the output should be
 swapAdjacentBits(n) = 14.
 
-1310 = 11012 ~> {11}{01}2 ~> 11102 = 1410.
+13_10 = 1101_2 ~> {11}{01}_2 ~> 1110_2 = 14_10.
 
 For n = 74, the output should be
 swapAdjacentBits(n) = 133.
 
-7410 = 010010102 ~> {01}{00}{10}{10}2 ~> 100001012 = 13310.
+74_10 = 01001010_2 ~> {01}{00}{10}{10}_2 ~> 10000101_2 = 133_10.
 Note the preceding zero written in front of the initial number: since both numbers are 32-bit integers, they have
 32 bits in their binary representation. The preceding zeros in other cases don't matter, so they are omitted.
 Here, however, it does make a difference.
