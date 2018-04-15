5ef572a5257677ea3560671466deab07d471ec4c
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 15:09:56 -0500

    Create willYou?.py

---
+++ core/atTheCrossroads/willYou?.py
@@ -0,0 +1,29 @@
+"""
+Once Mary heard a famous song, and a line from it stuck in her head. That line was "Will you still love me
+when I'm no longer young and beautiful?". Mary believes that a person is loved if and only if he/she is both
+young and beautiful, but this is quite a depressing thought, so she wants to put her belief to the test.
+
+Knowing whether a person is young, beautiful and loved, find out if they contradict Mary's belief.
+
+A person contradicts Mary's belief if one of the following statements is true:
+
+they are young and beautiful but not loved;
+they are loved but not young or not beautiful.
+Example
+
+For young = true, beautiful = true and loved = true, the output should be
+willYou(young, beautiful, loved) = false.
+
+Young and beautiful people are loved according to Mary's belief.
+
+For young = true, beautiful = false and loved = true, the output should be
+willYou(young, beautiful, loved) = true.
+
+Mary doesn't believe that not beautiful people can be loved.
+"""
+def willYou(young, beautiful, loved):
+    if young and beautiful and not loved:
+        return True
+    if loved and (not young or not beautiful):
+        return True
+    return False
