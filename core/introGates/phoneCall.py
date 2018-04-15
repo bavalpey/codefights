d38c44c84c5c4da7975184547423ddb139abdf72
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 14:59:17 -0500

    Update and rename core/Intro gates/phoneCall.py to core/introGates/phoneCall.py

--- core/introGates/phoneCall.py
+++ core/introGates/phoneCall.py
@@ -4,7 +4,8 @@ Some phone usage rate may be described as follows:
 first minute of a call costs min1 cents,
 each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
 each minute after 10th costs min11 cents.
-You have s cents on your account before the call. What is the duration of the longest call (in minutes rounded down to the nearest integer) you can have?
+You have s cents on your account before the call. What is the duration of the longest call
+(in minutes rounded down to the nearest integer) you can have?
 
 Example
 
@@ -14,7 +15,8 @@ phoneCall(min1, min2_10, min11, s) = 14.
 Here's why:
 
 the first minute costs 3 cents, which leaves you with 20 - 3 = 17 cents;
-the total cost of minutes 2 through 10 is 1 * 9 = 9, so you can talk 9 more minutes and still have 17 - 9 = 8 cents;
+the total cost of minutes 2 through 10 is 1 * 9 = 9, so you can talk 9 more minutes and still
+have 17 - 9 = 8 cents;
 each next minute costs 2 cents, which means that you can talk 8 / 2 = 4 more minutes.
 Thus, the longest call you can make is 1 + 9 + 4 = 14 minutes long.
 """
