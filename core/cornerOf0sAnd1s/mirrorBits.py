e2a2866c847ffeb2ef5c1db297aa282b94a3debb
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 15:16:54 -0500

    Create mirrorBits.py

---
+++ core/cornerOf0sAnd1s/mirrorBits.py
@@ -0,0 +1,15 @@
+"""
+Reverse the order of the bits in a given integer.
+
+Example
+
+For a = 97, the output should be
+mirrorBits(a) = 67.
+
+97 equals to 1100001 in binary, which is 1000011 after mirroring, and that is 67 in base 10.
+
+For a = 8, the output should be
+mirrorBits(a) = 1.
+"""
+def mirrorBits(a):
+    return int((bin(a)[2:])[::-1], 2)
