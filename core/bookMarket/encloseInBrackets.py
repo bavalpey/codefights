1d1acdba7586f00bd890fc4b93ffd3dd18fcccce
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 19:41:45 -0500

    Create encloseInBrackets.py

---
+++ core/bookMarket/encloseInBrackets.py
@@ -0,0 +1,10 @@
+"""
+Given a string, enclose it in round brackets.
+
+Example
+
+For inputString = "abacaba", the output should be
+  encloseInBrackets(inputString) = "(abacaba)".
+"""
+def encloseInBrackets(inputString):
+    return "(" + inputString + ")"
