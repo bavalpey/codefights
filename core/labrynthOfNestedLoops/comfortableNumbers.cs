296a9675c9f51b03bd975f64ca362b22d3443def
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-15 19:38:02 -0500

    Update comfortableNumbers.cs

--- core/labrynthOfNestedLoops/comfortableNumbers.cs
+++ core/labrynthOfNestedLoops/comfortableNumbers.cs
@@ -1,7 +1,9 @@
 /*
-Let's say that number a feels comfortable with number b if a ≠ b and b lies in the segment [a - s(a), a + s(a)], where s(x) is the sum of x's digits.
+Let's say that number a feels comfortable with number b if a ≠ b and b lies in the segment [a - s(a), a + s(a)], where s(x) is the
+sum of x's digits.
 
-How many pairs (a, b) are there, such that a < b, both a and b lie on the segment [l, r], and each number feels comfortable with the other?
+How many pairs (a, b) are there, such that a < b, both a and b lie on the segment [l, r], and each number feels comfortable with the
+other?
 
 Example
 
