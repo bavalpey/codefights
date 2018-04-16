8443ab0ae220f18b7c4ccd2b3971a30eee261b08
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-16 18:29:30 -0500

    Update countBits.py

--- python/meetPython/countBits.py
+++ python/meetPython/countBits.py
@@ -1,5 +1,12 @@
 """
+Implement a function that, given an integer n, uses a specific method on it and returns the number of bits in its binary representation.
 
+Example
+
+For n = 50, the output should be
+    countBits(n) = 6.
+
+5010 = 1100102, a number that consists of 6 digits. Thus, the output should be 6.
 """
 def countBits(n):
     return n.bit_length()
