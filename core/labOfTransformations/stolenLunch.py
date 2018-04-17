9df6fedf02df5f3c9292417ff77a10af06cd42f3
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-16 21:56:56 -0500

    Update stolenLunch.py

--- core/labOfTransformations/stolenLunch.py
+++ core/labOfTransformations/stolenLunch.py
@@ -12,4 +12,16 @@ For note = "you'll n4v4r 6u4ss 8t: cdja", the output should be
   stolenLunch(note) = "you'll never guess it: 2390".
 """
 def stolenLunch(note):
-    
+    my_dict = {'0':'a','1':'b','2':'c','3':'d','4':'e','5':'f','6':'g','7':'h','8':'i','9':'j'}
+    msg = ""
+    reverse = {}
+    for key in my_dict:
+        reverse[my_dict[key]] = key
+    for char in note:
+        if char in my_dict:
+            msg += my_dict[char]
+        elif char in reverse:
+            msg += reverse[char]
+        else:
+            msg += char
+    return msg
