aac4a20bfc270709e17b4e8d6312e269594ba833
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-21 21:04:33 -0500

    Create centuryFromYear.rb

---
+++ intro/theJourneyBegins/centuryFromYear.rb
@@ -0,0 +1,17 @@
+"""
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the
+year 101 up to and including the year 200, etc.
+
+Example
+
+  For year = 1905, the output should be
+    centuryFromYear(year) = 20;
+  For year = 1700, the output should be
+    centuryFromYear(year) = 17.
+"""
+def centuryFromYear(year)
+    unless year % 100 == 0
+        return year / 100 + 1
+    end
+    return year / 100
+end
