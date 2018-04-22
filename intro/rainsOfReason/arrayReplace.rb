9f2fd4b84caa424b72afc74614c2d4efc953c6af
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-21 21:34:04 -0500

    Update arrayReplace.rb

--- intro/rainsOfReason/arrayReplace.rb
+++ intro/rainsOfReason/arrayReplace.rb
@@ -1,11 +1,9 @@
-"""
-Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+# Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
 
-Example
+# Example
 
-For inputArray = [1, 2, 1], elemToReplace = 1 and substitutionElem = 3, the output should be
-  arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
-  """
+# For inputArray = [1, 2, 1], elemToReplace = 1 and substitutionElem = 3, the output should be
+#   arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
 def arrayReplace(a, e, s)
     a.map! {|i| i == e ? s:i}
     return a
