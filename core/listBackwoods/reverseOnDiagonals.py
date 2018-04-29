c04d53a89022ce705bc5dfddcf3207528714d8b7
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-04-29 18:05:56 -0500

    Update reverseOnDiagonals.py

--- core/listBackwoods/reverseOnDiagonals.py
+++ core/listBackwoods/reverseOnDiagonals.py
@@ -1,8 +1,9 @@
 """
 The longest diagonals of a square matrix are defined as follows:
 
-the first longest diagonal goes from the top left corner to the bottom right one;
-the second longest diagonal goes from the top right corner to the bottom left one.
+  the first longest diagonal goes from the top left corner to the bottom right one;
+  the second longest diagonal goes from the top right corner to the bottom left one.
+  
 Given a square matrix, your task is to reverse the order of elements on both of its longest diagonals.
 
 Example
