0c98d067086c3302c50e6125c4ee3b1304cffbd1
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-09-22 20:43:08 -0500

    Create chessKnightMoves.cpp

---
+++ core/sortingOutpost/chessKnightMoves.cpp
@@ -0,0 +1,28 @@
+/*
+Duplicate of intro level 11
+*/
+
+def chessKnightMoves(cell):
+    moves = 8
+       
+    
+    if cell[0] in ['a','h']:
+        moves -= 4
+        if cell[1] in ['1','8']:
+            moves -= 2
+        elif cell[1] in ['2','7']:
+            moves -= 1
+    
+    elif cell[0] in ['b','g']:
+        moves -= 2
+        if cell[1] in ['1','8']:
+            moves -= 3
+        elif cell[1] in ['2','7']:
+            moves -= 2
+    else:
+        if cell[1] in ['1','8']:
+            moves -= 4
+        elif cell[1] in ['2','7']:
+            moves -= 2
+    
+    return moves
