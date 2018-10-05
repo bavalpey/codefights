cab42651929cc7c3307a631a84e3126ddd8b7b82
bavalpey <36710691+bavalpey@users.noreply.github.com>
2018-10-05 11:42:29 -0500

    Create findProfession.py

---
+++ interviewPractice/treesBasic/findProfession.py
@@ -0,0 +1,25 @@
+/*
+Consider a special family of Engineers and Doctors. This family has the following rules:
+  Everybody has two children.
+  The first child of an Engineer is an Engineer and the second child is a Doctor.
+  The first child of a Doctor is a Doctor and the second child is an Engineer.
+  All generations of Doctors and Engineers start with an Engineer.
+We can represent the situation using this diagram:
+                E
+           /         \
+          E           D
+        /   \        /  \
+       E     D      D    E
+      / \   / \    / \   / \
+     E   D D   E  D   E E   D
+Given the level and position of a person in the ancestor tree above, find the profession of the person.
+Note: in this tree first child is considered as left child, second - as right.
+
+Example
+For level = 3 and pos = 3, the output should be
+  findProfession(level, pos) = "Doctor".
+*/
+def findProfession(level, pos):
+  if sum(int(i) for i in bin(pos-1)[2:]) %2:
+    return "Doctor"
+  return "Engineer"
